9313995c3ac83d825a859e22931eaee3bd59cc5c
GalibIbr <galibiavi4@gmail.com>
2023-11-20 17:13:08 +0300

    cownload cv 2

--- src/App.jsx
+++ src/App.jsx
@@ -100,7 +100,7 @@ const App = () => {
                 target="_blank"
                 className="bg-[#2d2e32] text-white flex items-center px-[22px] py-[14px] md:px-[26px] md:py-[17px] gap-[7px] rounded-[15px] text-[14px] hover:bg-black hover:scale-[0.95] duration-200">
                 <p className="font-[600]">Say Hello</p>
-                <FaTelegramPlane className="text-[20px]"/>
+                <FaTelegramPlane className="text-[20px]" />
               </a>
             </div>
           </div>
@@ -141,7 +141,9 @@ const App = () => {
       )}
       {/* About me */}
       <section className="max-w-[1000px] mx-auto px-[25px] mb-[50px]">
-        <p className="text-center md:text-[38px] text-[30px] font-[600]">About Me</p>
+        <p className="text-center md:text-[38px] text-[30px] font-[600]">
+          About Me
+        </p>
         <p className="text-center">My introduction</p>
         <div className="grid md:gap-0 gap-[40px] md:grid-cols-2 py-[40px] md:py-[60px]">
           <div className="md:pr-[60px] flex justify-center md:justify-start">
@@ -153,20 +155,26 @@ const App = () => {
           </div>
           <div className="flex flex-col justify-between gap-[25px] md:gap-0 text-center md:text-start">
             <div className="grid grid-cols-3 gap-[10px]">
-              <div className="border rounded-[8px] p-[20px] flex bg-white flex-col items-center">
+              <div className="border rounded-[8px] md:p-[20px] p-[10px] flex bg-white flex-col items-center">
                 <PiMedal className="text-[20px] md:text-[24px]" />
                 <p className="font-[600] md:text-[18px] mt-[4px]">Experience</p>
-                <p className="md:text-[14px] text-[11px] text-center w-full">3 + Years</p>
+                <p className="md:text-[14px] text-[11px] text-center w-full">
+                  3 + Years
+                </p>
               </div>
-              <div className="border rounded-[8px] p-[20px] flex bg-white flex-col items-center">
+              <div className="border rounded-[8px] md:p-[20px] p-[10px] flex bg-white flex-col items-center">
                 <MdOutlineWorkOutline className="text-[20px] md:text-[24px]" />
                 <p className="font-[600] md:text-[18px] mt-[4px]">Completed</p>
-                <p className="md:text-[14px] text-[11px] text-center w-full">14 + Projects</p>
+                <p className="md:text-[14px] text-[11px] text-center w-full">
+                  14 + Projects
+                </p>
               </div>
-              <div className="border rounded-[8px] p-[20px] flex bg-white flex-col items-center">
+              <div className="border rounded-[8px] md:p-[20px] p-[10px] flex bg-white flex-col items-center">
                 <BiSupport className="text-[20px] md:text-[24px]" />
                 <p className="font-[600] md:text-[18px] mt-[4px]">Support</p>
-                <p className="md:text-[14px] text-[11px] text-center w-full">Online 24/7</p>
+                <p className="md:text-[14px] text-[11px] text-center w-full">
+                  Online 24/7
+                </p>
               </div>
             </div>
             <p>
@@ -177,10 +185,11 @@ const App = () => {
             </p>
             <div className="flex justify-center md:justify-start">
               <a
-                href="/src/assets/Galib Ibragimov CV .pdf" download
+                href="/src/assets/Galib Ibragimov CV .pdf"
+                download="/src/assets/Galib Ibragimov CV .pdf"
                 className="bg-[#2d2e32] text-white flex items-center px-[22px] py-[14px] md:px-[26px] md:py-[17px] gap-[7px] rounded-[15px] text-[14px] hover:bg-black hover:scale-[0.95] duration-200">
                 <p className="font-[600]">Download CV</p>
-                <FaSheetPlastic className="text-[20px]"/>
+                <FaSheetPlastic className="text-[20px]" />
               </a>
             </div>
           </div>
