6310da8e99a857bfbd1e9802f8d618d22be8d827
GalibIbr <galibiavi4@gmail.com>
2023-11-21 16:39:31 +0300

    skills 03

--- src/App.jsx
+++ src/App.jsx
@@ -238,6 +238,96 @@ const App = () => {
                 <p className="text-[14px] leading-[0px]">55%</p>
               </div>
             </div>
+            <div className="grid grid-cols-[auto_1fr] grid-rows-[auto_auto]">
+              <div className="col-start-1 row-start-1 row-end-2 px-[10px] flex items-center">
+                <BsFillPatchCheckFill />
+              </div>
+              <p className="col-start-2 text-[18px] font-[600]">sss</p>
+              <div className="col-start-2 row-start-2 flex items-center gap-[5px]">
+                <div className="w-full bg-[#2d2e32] p-[2px] rounded-full">
+                  <div className="rounded-full bg-white">
+                    <div
+                      className={`w-[55%] bg-[#2d2e32] p-[3px]`}></div>
+                  </div>
+                </div>
+                <p className="text-[14px] leading-[0px]">60%</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-[auto_1fr] grid-rows-[auto_auto]">
+              <div className="col-start-1 row-start-1 row-end-2 px-[10px] flex items-center">
+                <BsFillPatchCheckFill />
+              </div>
+              <p className="col-start-2 text-[18px] font-[600]">sss</p>
+              <div className="col-start-2 row-start-2 flex items-center gap-[5px]">
+                <div className="w-full bg-[#2d2e32] p-[2px] rounded-full">
+                  <div className="rounded-full bg-white">
+                    <div
+                      className={`w-[55%] bg-[#2d2e32] p-[3px]`}></div>
+                  </div>
+                </div>
+                <p className="text-[14px] leading-[0px]">65%</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-[auto_1fr] grid-rows-[auto_auto]">
+              <div className="col-start-1 row-start-1 row-end-2 px-[10px] flex items-center">
+                <BsFillPatchCheckFill />
+              </div>
+              <p className="col-start-2 text-[18px] font-[600]">sss</p>
+              <div className="col-start-2 row-start-2 flex items-center gap-[5px]">
+                <div className="w-full bg-[#2d2e32] p-[2px] rounded-full">
+                  <div className="rounded-full bg-white">
+                    <div
+                      className={`w-[55%] bg-[#2d2e32] p-[3px]`}></div>
+                  </div>
+                </div>
+                <p className="text-[14px] leading-[0px]">70%</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-[auto_1fr] grid-rows-[auto_auto]">
+              <div className="col-start-1 row-start-1 row-end-2 px-[10px] flex items-center">
+                <BsFillPatchCheckFill />
+              </div>
+              <p className="col-start-2 text-[18px] font-[600]">sss</p>
+              <div className="col-start-2 row-start-2 flex items-center gap-[5px]">
+                <div className="w-full bg-[#2d2e32] p-[2px] rounded-full">
+                  <div className="rounded-full bg-white">
+                    <div
+                      className={`w-[55%] bg-[#2d2e32] p-[3px]`}></div>
+                  </div>
+                </div>
+                <p className="text-[14px] leading-[0px]">75%</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-[auto_1fr] grid-rows-[auto_auto]">
+              <div className="col-start-1 row-start-1 row-end-2 px-[10px] flex items-center">
+                <BsFillPatchCheckFill />
+              </div>
+              <p className="col-start-2 text-[18px] font-[600]">sss</p>
+              <div className="col-start-2 row-start-2 flex items-center gap-[5px]">
+                <div className="w-full bg-[#2d2e32] p-[2px] rounded-full">
+                  <div className="rounded-full bg-white">
+                    <div
+                      className={`w-[55%] bg-[#2d2e32] p-[3px]`}></div>
+                  </div>
+                </div>
+                <p className="text-[14px] leading-[0px]">80%</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-[auto_1fr] grid-rows-[auto_auto]">
+              <div className="col-start-1 row-start-1 row-end-2 px-[10px] flex items-center">
+                <BsFillPatchCheckFill />
+              </div>
+              <p className="col-start-2 text-[18px] font-[600]">sss</p>
+              <div className="col-start-2 row-start-2 flex items-center gap-[5px]">
+                <div className="w-full bg-[#2d2e32] p-[2px] rounded-full">
+                  <div className="rounded-full bg-white">
+                    <div
+                      className={`w-[55%] bg-[#2d2e32] p-[3px]`}></div>
+                  </div>
+                </div>
+                <p className="text-[14px] leading-[0px]">75%</p>
+              </div>
+            </div>
           </div>
         </div>
       </section>
