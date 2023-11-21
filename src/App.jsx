446eee15f64cd08d175ee7e3ab22bf740f840e9b
GalibIbr <galibiavi4@gmail.com>
2023-11-21 16:44:56 +0300

    skills 05

--- src/App.jsx
+++ src/App.jsx
@@ -247,6 +247,21 @@ const App = () => {
                 <div className="w-full bg-[#2d2e32] p-[2px] rounded-full">
                   <div className="rounded-full bg-white">
                     <div
+                      className={`w-[50%] bg-[#2d2e32] p-[3px]`}></div>
+                  </div>
+                </div>
+                <p className="text-[14px] leading-[0px]">55%</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-[auto_1fr] grid-rows-[auto_auto]">
+              <div className="col-start-1 row-start-1 row-end-2 px-[10px] flex items-center">
+                <BsFillPatchCheckFill />
+              </div>
+              <p className="col-start-2 text-[18px] font-[600]">sss</p>
+              <div className="col-start-2 row-start-2 flex items-center gap-[5px]">
+                <div className="w-full bg-[#2d2e32] p-[2px] rounded-full">
+                  <div className="rounded-full bg-white">
+                    <div
                       className={`w-[60%] bg-[#2d2e32] p-[3px]`}></div>
                   </div>
                 </div>
@@ -307,7 +322,7 @@ const App = () => {
                 <div className="w-full bg-[#2d2e32] p-[2px] rounded-full">
                   <div className="rounded-full bg-white">
                     <div
-                      className={`w-[50%] bg-[#2d2e32] p-[3px]`}></div>
+                      className={`w-[80%] bg-[#2d2e32] p-[3px]`}></div>
                   </div>
                 </div>
                 <p className="text-[14px] leading-[0px]">80%</p>
