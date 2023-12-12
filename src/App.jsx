c91604c53ab34c1300a019268d9dc0e1c6592f44
GalibIbr <galibiavi4@gmail.com>
2023-12-12 18:09:12 +0300

    carland project

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import { BsFillPatchCheckFill } from "react-icons/bs";
 import amazon from "./assets/projects/amazon.png";
 import amazon2 from "./assets/projects/amazon2.png";
 import flash_pizza from "./assets/projects/frash-pizza.png";
+import carland from "./assets/projects/carland.png";
 import { FiGithub } from "react-icons/fi";
 import { RiExternalLinkLine } from "react-icons/ri";
 import { PiStudentBold } from "react-icons/pi";
@@ -604,8 +605,77 @@ const App = () => {
               </div>
             </div>
           </div>
+          {/* carland */}
+          {/* <div className="p-[20px] div-amazon rounded-[20px] grid md:grid-cols-[60%_40%] bg-white"> */}
+          <div className="p-[20px] div-carland rounded-[20px] flex flex-col-reverse md:grid md:grid-cols-[40%_60%] bg-white">
+            {/* info */}
+            <div className="text-center flex md:text-[17px] pt-[20px] md:pt-0 gap-[8px] flex-col justify-around md:pr-[40px] md:pl-[20px]">
+              <p>
+                <b>CARLAND</b>{" "}
+                <span className="text-[14px]">(DECEMBER 2023)</span>
+              </p>
+              <p className="leading-[22px]">
+                A car rental website is an online platform that allows users to
+                rent cars for personal or business use. The website provides an
+                interface for searching, comparing, and reserving cars.
+              </p>
+              {/* skills */}
+              <div className="flex flex-wrap text-[16px] justify-center gap-y-[5px] gap-x-[15px]">
+                <p className="flex gap-[3px]">
+                  <img
+                    className="w-[20px]"
+                    src="https://skillicons.dev/icons?i=nextjs"
+                  />
+                  <b>Next.js</b>
+                </p>
+                <p className="flex gap-[3px]">
+                  <img
+                    className="w-[20px]"
+                    src="https://skillicons.dev/icons?i=javascript"
+                  />
+                  <b>JavaScript</b>
+                </p>
+                <p className="flex gap-[3px]">
+                  <img
+                    className="w-[20px]"
+                    src="https://skillicons.dev/icons?i=tailwind"
+                  />
+                  <b>Tailwind CSS</b>
+                </p>
+              </div>
+              <div className="flex justify-evenly">
+                <a
+                  href="https://github.com/galibibr/carland"
+                  target="_blank"
+                  className="flex items-center gap-[5px] hover:text-black hover:scale-[1.05] duration-300">
+                  Code <FiGithub className="text-[22px]" />
+                </a>
+                <a
+                  href="https://carland-tau.vercel.app/"
+                  target="_blank"
+                  className="flex items-center gap-[5px] hover:text-black hover:scale-[1.05] duration-300">
+                  Live Demo <RiExternalLinkLine className="text-[24px]" />
+                </a>
+              </div>
+            </div>
+            {/* photo */}
+            <a
+              href="https://carland-tau.vercel.app/"
+              target="_blank"
+              className="w-auto h-[220px] md:h-[360px] box-amazon overflow-hidden rounded-[20px]">
+              <img src={carland} className="img-carland" alt="amazon web" />
+            </a>
+          </div>
           {/* frash pizza */}
-          <div className="p-[20px] div-amazon rounded-[20px] flex flex-col-reverse md:grid md:grid-cols-[40%_60%] bg-white">
+          {/* <div className="p-[20px] div-amazon rounded-[20px] flex flex-col-reverse md:grid md:grid-cols-[40%_60%] bg-white"> */}
+          <div className="p-[20px] div-amazon rounded-[20px] grid md:grid-cols-[60%_40%] bg-white">
+            {/* photo */}
+            <a
+              href="https://flash-pizza.vercel.app/"
+              target="_blank"
+              className="w-auto h-[220px] md:h-[360px] box-amazon overflow-hidden rounded-[20px]">
+              <img src={flash_pizza} className="img-amazon" alt="amazon web" />
+            </a>
             {/* info */}
             <div className="text-center flex md:text-[17px] pt-[20px] md:pt-0 gap-[8px] flex-col justify-around md:pl-[40px] md:pr-[20px]">
               <p>
@@ -664,13 +734,6 @@ const App = () => {
                 </a>
               </div>
             </div>
-            {/* photo */}
-            <a
-              href="https://flash-pizza.vercel.app/"
-              target="_blank"
-              className="w-auto h-[220px] md:h-[360px] box-amazon overflow-hidden rounded-[20px]">
-              <img src={flash_pizza} className="img-amazon" alt="amazon web" />
-            </a>
           </div>
         </div>
       </section>
