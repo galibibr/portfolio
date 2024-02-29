7151871b7492c3b41515c055610331f4c6fd4e86
Gholibjoni Ibrohimzoda <74533791+galibibr@users.noreply.github.com>
2024-02-29 11:29:27 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -848,7 +848,7 @@ const App = () => {
       {/* Footer */}
       <footer className="md:py-[50px] py-[20px] px-[25px] bg-[#2d2e32]">
         <p className="text-center text-white font-[500] md:text-[14px] text-[12px]">
-          Copyright © 2023. All rights are reserved
+          Copyright © 2024. All rights are reserved
         </p>
       </footer>
     </div>
